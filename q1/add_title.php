edf0b40866fdc3737e5e898c0c3b168992c707ba
YIHONG <akashidaiki2@gmail.com>
2023-11-30 14:24:18 +0800

    完成新增標題資料功能

--- q1/add_title.php
+++ q1/add_title.php
@@ -1,5 +1,12 @@
 <?php
+include_once "db.php";
 
 if (!empty($_FILES['img']['tmp_name'])) {
   move_uploaded_file($_FILES['img']['tmp_name'], "./img/" . $_FILES['img']['tmp_name']);
+  $_POST['img'] = $FILES['img']['name'];
 }
+$_POST['sh'] = 0;
+
+$Title->save($_POST);
+
+header("location:index.php");
