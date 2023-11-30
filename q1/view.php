1d273e453d1996dc1ab33efed37e7f0207e2970a
YIHONG <akashidaiki2@gmail.com>
2023-11-30 13:57:28 +0800

    建立彈出視窗檔案

---
+++ q1/view.php
@@ -0,0 +1,18 @@
+<h3 class="text-center mt-3">新增標題區圖片</h3>
+<hr>
+<form action="" method="post" enctype="multipart/form-data">
+  <table class="col-8 m-auto">
+    <tr>
+      <td>標題區圖片：</td>
+      <td><input type="file" name="img" id=""></td>
+    </tr>
+    <tr>
+      <td>標題區替代文字：</td>
+      <td><input type="text" name="text" id=""></td>
+    </tr>
+  </table>
+  <div class="text-center">
+    <input type="submit" value="新增">
+    <input type="reset" value="重置">
+  </div>
+</form>
