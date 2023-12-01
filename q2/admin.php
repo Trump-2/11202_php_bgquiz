c4f789812453ee458118b656d4eff61de9d4e808
YIHONG <akashidaiki2@gmail.com>
2023-12-01 09:51:07 +0800

    完成問卷調查後面頁面

--- q2/admin.php
+++ q2/admin.php
@@ -13,7 +13,30 @@
   <header class="container p-5">
     <h1 class="text-center">問卷管理</h1>
   </header>
-  <main class="container p-3"></main>
+  <main class="container p-3">
+    <fieldset>
+      <legend>新增問卷</legend>
+      <form action="add_question.php" method="post">
+        <div class="d-flex">
+          <div class="col-3 bg-light p-2">問卷名稱</div>
+          <div class="col-6 p-2">
+            <input type="text" name="subject" id="">
+          </div>
+        </div>
+        <div class="bg-light">
+          <div class=p-2>
+            <label for="">選項</label>
+            <input type="text" name="option[]" id="">
+            <input type="button" value="更多">
+          </div>
+        </div>
+        <div>
+          <input type="submit" value="新增">
+          <input type="reset" value="清空">
+        </div>
+      </form>
+    </fieldset>
+  </main>
   <script src="../js/jquery-3.4.1.min.js"></script>
   <script src="../js/bootstrap.js"></script>
 
