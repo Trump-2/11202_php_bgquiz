b7d7fc3db372f5827eca4f09aa0112a4ebff42d1
YIHONG <akashidaiki2@gmail.com>
2023-12-01 09:27:43 +0800

    建立第二題問卷調查顯示頁面

---
+++ q2/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>問卷調查</title>
+  <link rel="stylesheet" href="../css/bootstrap.css">
+</head>
+
+<body>
+  <header class="p-5">
+    <h1 class="text-center">問卷調查</h1>
+  </header>
+  <main class="container">
+    <fieldset>
+      <legend>目前位置：首頁 > 問卷調查</legend>
+      <table>
+        <tr>
+          <th>編號</th>
+          <th>問卷題目</th>
+          <th>投票總數</th>
+          <th>結果</th>
+          <th>狀態</th>
+        </tr>
+        <tr>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+          <td></td>
+        </tr>
+      </table>
+    </fieldset>
+  </main>
+  <script src="../js/jquery-3.4.1.min.js"></script>
+  <script src="../js/bootstrap.js"></script>
+</body>
+
+</html>
